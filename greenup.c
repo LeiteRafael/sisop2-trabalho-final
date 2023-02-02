5566422e2e6c54ff763e0dc961473f287e9ab944
Thiago Santos <49589136+ThiagoSzz@users.noreply.github.com>
2023-02-01 22:43:27 -0300

    added broadcast, but not working properly
    guest sends the message to the manager (working fine)
    but guest can't get the "ack" message from the manager

--- greenup.c
+++ greenup.c
@@ -6,9 +6,21 @@
 #include <sys/socket.h>
 
 #define DISCOVERY_PORT 9999
-#define MAX_MSG_LEN 256
+#define MAX_MSG_LEN 65536
+
+
+typedef struct __packet{
+    uint16_t type;        // Tipo do pacote (p.ex. DATA | CMD)
+    uint16_t seqn;        // Número de sequência
+    uint16_t length;      // Comprimento do payload
+    uint16_t timestamp;   // Timestamp do dado
+    const char* _payload; // Dados da mensagem
+} packet;
+
 
 int main(int argc, char *argv[]) {
+    packet msg_packet;
+
     int discovery_socket;
     char discovery_buf[MAX_MSG_LEN];
     struct sockaddr_in discovery_addr;
@@ -22,11 +34,14 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
+    int broadcastEnable = 1;
+    setsockopt(discovery_socket, SOL_SOCKET, SO_BROADCAST, &broadcastEnable, sizeof(broadcastEnable));
+
     if(strcmp(argv[1],"manager") == 0){
         memset(&discovery_addr, 0, sizeof(discovery_addr));
         discovery_addr.sin_family = AF_INET;
-        discovery_addr.sin_addr.s_addr = htonl(INADDR_ANY);
         discovery_addr.sin_port = htons(DISCOVERY_PORT);
+        discovery_addr.sin_addr.s_addr = inet_addr("255.255.255.255");
 
         if (bind(discovery_socket, (struct sockaddr *) &discovery_addr, sizeof(discovery_addr)) < 0) {
             perror("Falhou no binding.");
@@ -48,21 +63,24 @@ int main(int argc, char *argv[]) {
         //close(discovery_socket);
     }
     else{
+        char* discovery_msg;
+            
         // ip e porta do líder
         memset(&leader_addr, 0, sizeof(leader_addr));
         leader_addr.sin_family = AF_INET;
         leader_addr.sin_port = htons(DISCOVERY_PORT);
-        leader_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+        leader_addr.sin_addr.s_addr = inet_addr("255.255.255.255");
 
         // envia mensagem para o líder
-        char discovery_msg[] = "Nova maquina conectada.";
-        sendto(discovery_socket, discovery_msg, sizeof(discovery_msg), 0, (struct sockaddr *) &leader_addr, sizeof(leader_addr));
+        msg_packet._payload = "Mensagem aqui.";
+        sendto(discovery_socket, msg_packet._payload, sizeof(msg_packet), 0, (struct sockaddr *) &leader_addr, sizeof(leader_addr));
 
         // aqui seria onde vai receber o pacote WoL
         while (1) {
             int bytes_received = recvfrom(discovery_socket, discovery_msg, sizeof(discovery_msg), 0, (struct sockaddr *) &leader_addr, &addr_len_l);
 
             if (bytes_received > 0) {
+                bytes_received = 0;
                 printf("%s\n", discovery_msg);
                 break;
             }
